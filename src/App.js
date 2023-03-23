403b3182af993f5ea5a469999de5926715b65cc8
Aaron <schwarzerheisserkaffee@gmx.de>
2023-03-23 23:58:36 +0100

    Add component and icon library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.scss';
 import BeerBackground from './components/beer-background';
+import { Gi3DStairs } from "react-icons/gi";
 
 function App() {
   return (
     <div className="App">
       <h1 className="bigHeading">Gigantoball</h1>
+      <div className='whiteee'><Gi3DStairs/></div>
       <div className='BeerBackground'>
         <BeerBackground/>
       </div>
